149e2ad78a833eb207683ae0f0682b826875b867
BDan0025 <51334675+BDan0025@users.noreply.github.com>
2026-07-19 18:23:05 +0200

    Add integration manifest and constants

---
+++ custom_components/kreis_steinfurt/const.py
@@ -0,0 +1,14 @@
+"""Constants for Kreis Steinfurt integration."""
+
+DOMAIN = "kreis_steinfurt"
+
+NAME = "Kreis Steinfurt Einsätze"
+
+DEFAULT_SCAN_INTERVAL = 60
+
+SOURCE_URL = (
+    "https://www.kreis-steinfurt.de/"
+    "kv_steinfurt/Kreisverwaltung/"
+    "%C3%84mter/Amt%20f%C3%BCr%20Bev%C3%B6lkerungsschutz/"
+    "Kreisleitstelle/Aktuelle%20Eins%C3%A4tze/"
+)
