36dfbd4c460d1eb08d78ab12cf043fe5dc08be0a
BDan0025 <51334675+BDan0025@users.noreply.github.com>
2026-07-19 18:28:14 +0200

    Add Home Assistant integration entry point

---
+++ custom_components/kreis_steinfurt/__init__.py
@@ -0,0 +1,36 @@
+"""Kreis Steinfurt Einsätze integration."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    """Set up the integration from YAML."""
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up Kreis Steinfurt from a config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Unload a config entry."""
+
+    hass.data.pop(DOMAIN, None)
+
+    return True
